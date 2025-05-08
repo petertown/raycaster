d1959a1b8128582b65febe64cda6bfbe2226ebc3
Peter <magicacid@outlook.com>
2025-05-08 20:03:20 +1000

    Rename the folders to make it easier to know what is what

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { KimlabComponent } from '../features/kimlab/kimlab.component';
-import { OrthagonalComponent } from '../features/orthagonal/orthagonal.component';
-import { RaycasterComponent } from '../features/raycaster/raycaster.component';
+import { OrthagonalComponent } from '../features/raycasterv1/orthagonal.component';
+import { RaycasterComponent } from '../features/raycasterv2/raycaster.component';
 
 export const routes: Routes = [
   {
--- src/features/kimlab/kimlab.component.ts
+++ src/features/kimlab/kimlab.component.ts
@@ -8,8 +8,15 @@ import { GameState } from '../../model/gamestate.enum';
   styleUrl: './kimlab.component.scss',
 })
 export class KimlabComponent {
+  // I should make a "game state util" ?
   gameStateList!: GameState[];
+
+
   // Need to do a few things
   // Start with a menu
   // new game, continue, settings
+  // So that's first thing to do
+  // Menu's should be html overlaid over the top of the canvas, and should be able to be opened as a game state
+  // So, start with a main menu animation on the canvas
+  // Then when that menu animation ends, swap to the main menu over the top of it
 }
